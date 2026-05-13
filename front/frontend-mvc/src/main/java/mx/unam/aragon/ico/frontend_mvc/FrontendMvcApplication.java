17f016df7764c3627e78f7289abd504d1451a59f
Edgartor321 <edgar18tor@gmail.com>
2026-05-12 20:49:17 -0600

    Agrego proyecto spring de la parte del Frontend, que incluye las dependencias: Lombok Spring Web y Thtmeleaf.

---
+++ front/frontend-mvc/src/main/java/mx/unam/aragon/ico/frontend_mvc/FrontendMvcApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.frontend_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendMvcApplication.class, args);
+	}
+
+}
---
+++ front/frontend-mvc/src/test/java/mx/unam/aragon/ico/frontend_mvc/FrontendMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.frontend_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
